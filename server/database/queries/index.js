8d6d99b3d1ae33215b0f2aaa5081369e10760723
lina-jamal <linahjamal89@gmail.com>
2020-12-09 01:25:26 +0200

    create database queries file

---
+++ server/database/queries/index.js
@@ -0,0 +1,17 @@
+const connection = require("../config/connection");
+
+const getStudent = () => {};
+const getSubject = () => {};
+
+const addStudentQuery = (firstName, secondName, location) => {};
+
+const getStudentsSubject = () => {};
+const deleteStudentById = (id) => {};
+
+module.exports = {
+  getStudent,
+  getSubject,
+  getStudentsSubject,
+  addStudentQuery,
+  deleteStudentById,
+};

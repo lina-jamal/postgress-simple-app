6c4bbb9af611c795ccdd3721bd1d56a9effa8fd0
lina-jamal <linahjamal89@gmail.com>
2020-12-09 01:32:57 +0200

    build the database

--- server/database/config/build.js
+++ server/database/config/build.js
@@ -1,7 +1,16 @@
+const { readFileSync } = require("fs");
+const { join } = require("path");
+
 //import the connection
+const connection = require("./connection");
 
 //make db build
 // const dbBuild = () => {}
-const dbBuild = () => {};
+const dbBuild = () => {
+  const dbFile = readFileSync(join(__dirname, "student.sql")).toString();
+  const fakeData = readFileSync(join(__dirname, "fakedata.sql")).toString();
+
+  return connection.query(dbFile).then(() => connection.query(fakeData));
+};
 
 module.exports = dbBuild;

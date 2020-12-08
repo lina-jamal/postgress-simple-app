7a57358db9b62b9b9b4ae5ddefe66cf50e890a94
lina-jamal <linahjamal89@gmail.com>
2020-12-09 01:26:02 +0200

    create student controller

---
+++ server/controller/index.js
@@ -0,0 +1,38 @@
+const {
+  getStudent,
+  getSubject,
+  getStudentsSubject,
+  addStudentQuery,
+  deleteStudentById,
+} = require("../database/queries");
+
+const students = [
+  {
+    first_name: "Ali",
+    second_name: "Ali",
+    location: "Gaza",
+  },
+  {
+    first_name: "Shireen",
+    second_name: "Shireen",
+    location: "Nazareth",
+  },
+];
+const getStudents = (req, res, next) => {
+  res.send({
+    data: students,
+    statusCode: 200,
+  });
+};
+
+const getSusbject = (req, res, next) => {};
+const addStudent = (req, res, next) => {};
+
+const deleteStudent = (req, res, next) => {};
+
+module.exports = {
+  getSusbject,
+  getStudents,
+  addStudent,
+  deleteStudent,
+};
--- server/router.js
+++ server/router.js
@@ -1,20 +1,28 @@
 const router = require("express").Router();
 const path = require("path");
-
+const {
+  getSusbject,
+  getStudents,
+  addStudent,
+  deleteStudent,
+} = require("./controller");
 /*
-GET: /Notes
-POST: /Note
+GET: /students 
+POST: /student
+delete /student/:id
 */
 
-// router.get("/Notes", (req, res, next) => {});
-
-// router.post("/note", (req, res, next) => {});
+router.get("/students", getStudents);
+router.get("/subjects", getSusbject);
+router.post("/student", addStudent);
+router.delete("/student/:id", deleteStudent);
 
 router.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "public", "404.html"));
 });
 
 router.use((err, req, res, next) => {
+  console.log(err);
   res.status(500).send({ message: "internal server error" });
 });
 
